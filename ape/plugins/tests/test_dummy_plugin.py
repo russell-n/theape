6a4bf516c71029dd975bc5a2e040a9a0da08cc02
necromuralist <necromuralist@google.com>
2014-02-03 12:15:34 -0800

    Adding tests for dummy and composite plugins. (composite plugin not working as it expects a watcher class that doesn't exist.

---
+++ ape/plugins/tests/test_dummy_plugin.py
@@ -0,0 +1,36 @@
+
+# python standard library
+import unittest
+
+# third-party
+from mock import MagicMock, call
+
+# the ape
+from ape.plugins.dummyplugin import Dummy
+
+
+class TestingDummyPlugin(unittest.TestCase):
+    def setUp(self):
+        self.config_map = MagicMock()
+        self.section_header = 'somedummy'
+        self.plugin = Dummy(configuration=self.config_map,
+                           section_header=self.section_header)
+        return
+
+    def test_constructor(self):
+        """
+        Does it take the two expected parameters?
+        """
+        self.assertIs(self.config_map, self.plugin.configuration)
+        self.assertIs(self.section_header, self.plugin.section_header)
+        return
+
+    def test_product(self):
+        """
+        Does it build the product in the way you expect?
+        """
+        product = self.plugin.product
+        self.config_map.items.assert_called_with(section=self.section_header,
+                                                        optional=True,
+                                                        default={})
+        return    
